52ea6c3edd0cc2519d0ad4792db34d761569f159
bpseitz <72680355+bpseitz@users.noreply.github.com>
2020-12-21 18:40:11 -0800

    Create insertChallenges.sql

---
+++ insertChallenges.sql
@@ -0,0 +1,6 @@
+INSERT INTO `bananatestdb`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `CreatedAt`, `UpdatedAt`) VALUES ('Mount Everest', 'Run', 'Elevation Gain', '29032', current_timestamp(), current_timestamp());
+INSERT INTO `bananatestdb`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `CreatedAt`, `UpdatedAt`) VALUES ('Mount Everest', 'Bike', 'Elevation Gain', '29032', current_timestamp(), current_timestamp());
+INSERT INTO `bananatestdb`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `CreatedAt`, `UpdatedAt`) VALUES ('Pacific Crest Trail', 'Run', 'Distance', '4263850', current_timestamp(), current_timestamp());
+INSERT INTO `bananatestdb`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `CreatedAt`, `UpdatedAt`) VALUES ('NY to LA', 'Bike', 'Distance', '4489110', current_timestamp(), current_timestamp());
+INSERT INTO `bananatestdb`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `CreatedAt`, `UpdatedAt`) VALUES ('English Channel', 'Swim', 'Distance', '33789', current_timestamp(), current_timestamp());
+INSERT INTO `bananatestdb`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `CreatedAt`, `UpdatedAt`) VALUES ('Cuba to Florida', 'Swim', 'Distance', '176990', current_timestamp(), current_timestamp());
