f9f3092b29c4f886ad4b6c7b72ab14be056d94e6
bpseitz <72680355+bpseitz@users.noreply.github.com>
2020-12-22 18:34:57 -0800

    Add images and style Index View

--- insertChallenges.sql
+++ insertChallenges.sql
@@ -4,3 +4,13 @@ INSERT INTO `bananatestdb`.`challenges` (`Title`, `ActivityType`, `ChallengeType
 INSERT INTO `bananatestdb`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `CreatedAt`, `UpdatedAt`) VALUES ('NY to LA', 'Bike', 'Distance', '4489110', current_timestamp(), current_timestamp());
 INSERT INTO `bananatestdb`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `CreatedAt`, `UpdatedAt`) VALUES ('English Channel', 'Swim', 'Distance', '33789', current_timestamp(), current_timestamp());
 INSERT INTO `bananatestdb`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `CreatedAt`, `UpdatedAt`) VALUES ('Cuba to Florida', 'Swim', 'Distance', '176990', current_timestamp(), current_timestamp());
+
+
+
+
+UPDATE `bananatestdb`.`challenges` SET `ImageFileName` = 'everest.jpg' WHERE (`ChallengeId` = '1');
+UPDATE `bananatestdb`.`challenges` SET `ImageFileName` = 'pct.jpg' WHERE (`ChallengeId` = '2');
+UPDATE `bananatestdb`.`challenges` SET `ImageFileName` = 'losAngeles.jpg' WHERE (`ChallengeId` = '3');
+UPDATE `bananatestdb`.`challenges` SET `ImageFileName` = 'everest2.jpg' WHERE (`ChallengeId` = '4');
+UPDATE `bananatestdb`.`challenges` SET `ImageFileName` = 'englishChannel.jpg' WHERE (`ChallengeId` = '5');
+UPDATE `bananatestdb`.`challenges` SET `ImageFileName` = 'floridaBeach.jpg' WHERE (`ChallengeId` = '6');
