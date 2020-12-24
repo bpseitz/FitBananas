fc4f41c3279d55e5940796635d874b86e07e2a25
HBDanks <danksharold@gmail.com>
2020-12-24 10:16:40 -0800

    Add Security Middleware
    -Added security middleware to protect site from users trying to bypass strava OAuth

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -48,6 +49,26 @@ namespace FitBananas
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSession();
+            // Middleware to allow protect the site from users trying to bypass Strava authentication
+            app.Use(async (context, next) =>
+            {
+                // Figure out how to allow post request from the strava database only.
+                // there should be a value in the response that will tell us where the information is coming from.
+                // It should be in the context.request.headers
+                if(context.Request.Method == "Post")
+                {
+                    
+                }
+                // if a user is not signed in, they will be redirected back to the splash page
+                else if(context.Request.Path != "" && context.Session.GetInt32("UserId") == null)
+                {
+                    // reassigns the path to "/" before passing it back to the controller in "app.UseMvc"
+                    context.Request.Path = "/";
+                    Console.WriteLine($"Attempted server breach averted at {DateTime.Now}");
+                }
+                await next();
+
+            });
             app.UseMvc();
 
             // app.UseRouting();
