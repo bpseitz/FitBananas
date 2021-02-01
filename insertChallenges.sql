d5bb006e6debf839b6d27aa8c1356fc3746612e9
HBDanks <danksharold@gmail.com>
2021-02-01 12:19:09 -0800

    Add Challenge Images
    - updated ImageFileName for each challenge to accurately represent file name

--- insertChallenges.sql
+++ insertChallenges.sql
@@ -1,55 +1,55 @@
 ----- Run Gain -----
 
-INSERT INTO `bananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
+INSERT INTO `fitbananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
 VALUES ('Mount Everest', 'Run', 'Elevation Gain', '8849', 'everest.jpg', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
-INSERT INTO `bananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
-VALUES ('Olympus Mons', 'Run', 'Elevation Gain', '21900', '', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP())
+INSERT INTO `fitbananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
+VALUES ('Olympus Mons', 'Run', 'Elevation Gain', '21900', 'olympusMons.jpg', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
-INSERT INTO `bananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
-VALUES ('Denali', 'Run', 'Elevation Gain', '5900', '', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP())
+INSERT INTO `fitbananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
+VALUES ('Denali', 'Run', 'Elevation Gain', '5900', 'denali.jpg', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
 ----- Run Distance -----
 
-INSERT INTO `bananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
+INSERT INTO `fitbananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
 VALUES ('Pacific Crest Trail', 'Run', 'Distance', '4263850', 'pct.jpg', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
-INSERT INTO `bananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
-VALUES ('Appalachian Trail', 'Run', 'Distance', '3524463', '', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP())
+INSERT INTO `fitbananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
+VALUES ('Appalachian Trail', 'Run', 'Distance', '3524463', 'appalachianTrail.jpg', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP())
 
-INSERT INTO `bananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
-VALUES ('London to Rome', 'Run', 'Distance', '1880518', '', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP())
+INSERT INTO `fitbananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
+VALUES ('London to Rome', 'Run', 'Distance', '1880518', 'rome.jpg', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
 ----- Bike Gain -----
 
-INSERT INTO `bananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
+INSERT INTO `fitbananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
 VALUES ('Mount Everest', 'Bike', 'Elevation Gain', '8849', 'everest2.jpg', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
-INSERT INTO `bananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
-VALUES ('Olympus Mons', 'Bike', 'Elevation Gain', '21900', '', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP())
+INSERT INTO `fitbananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
+VALUES ('Olympus Mons', 'Bike', 'Elevation Gain', '21900', 'olympusMons.jpg', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
 ----- Bike Distance -----
 
-INSERT INTO `bananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
+INSERT INTO `fitbananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
 VALUES ('NY to LA', 'Bike', 'Distance', '4489110', 'losAngeles.jpg', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
-INSERT INTO `bananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
-VALUES ('Across Africa', 'Bike', 'Distance', '7400000', '', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP())
+INSERT INTO `fitbananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
+VALUES ('Across Africa', 'Bike', 'Distance', '7400000', 'africa.jpg', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
-INSERT INTO `bananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
-VALUES ('Seattle to Miami', 'Bike', 'Distance', '5306007', '', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP())
+INSERT INTO `fitbananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
+VALUES ('Seattle to Miami', 'Bike', 'Distance', '5306007', 'miami.jpg', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
 
 ----- Swim -----
 
-INSERT INTO `bananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
+INSERT INTO `fitbananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
 VALUES ('English Channel', 'Swim', 'Distance', '33789', 'englishChannel.jpg', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
-INSERT INTO `bananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
+INSERT INTO `fitbananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
 VALUES ('Cuba to Florida', 'Swim', 'Distance', '176990', 'floridaBeach.jpg', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
-INSERT INTO `bananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
-VALUES ('Jordan River', 'Swim', 'Distance', '251058', '', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+INSERT INTO `fitbananatest2db`.`challenges` (`Title`, `ActivityType`, `ChallengeType`, `Goal`, `ImageFileName`, `CreatedAt`, `UpdatedAt`) 
+VALUES ('Jordan River', 'Swim', 'Distance', '251058', 'jordanRiver.jpg', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
 
 
