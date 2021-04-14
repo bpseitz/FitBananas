881b047a2f3b37f8b225336ee512dcd76909ea6e
HBDanks <danksharold@gmail.com>
2021-04-13 22:09:16 -0700

    Update Challenge Model and NewChallengeViewModel
    -Begin to resolve Issues with safari by parsing Challenge List
    - Need to convert to a type of object that we can properly pass forward
        - Currently can't part to array in javascript

--- Models/Challenge.cs
+++ Models/Challenge.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace FitBananas.Models
 {
@@ -24,6 +25,9 @@ namespace FitBananas.Models
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
 
+        // This comes from Newtonsoft and allows us to prevent a referencing loop when serializing
+        // We need to come up with a better fix for this, we could create a view model specific to challenges and Athletes I believe - TD
+        [JsonIgnore]
         public List<AthleteChallenge> Athletes { get; set; }
 
         public int GoalToImperial()
--- Models/NewChallengeViewModel.cs
+++ Models/NewChallengeViewModel.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
 
 namespace FitBananas.Models
 {
@@ -8,12 +10,16 @@ namespace FitBananas.Models
     {
         public List<Challenge> AllChallenges { get; set; }
         public int AthleteId { get; set; }
+
+        public string JsonChallenges { get; set; }
         public NewChallengeViewModel(BananaContext context, int athleteId)
         {
             AllChallenges = context.Challenges
                 .Include(challenge => challenge.Athletes)
                     .ThenInclude(athlete => athlete.ThisAthlete)
                 .ToList();
+
+            JsonChallenges = JsonConvert.SerializeObject(AllChallenges);
             AthleteId = athleteId;
         }
     }
